58c32d8472baae7b36dd38af3e0dd4f67fe3ea1d
Luis Felipe Gonzalez Moreno <luisfgonzalez91@gmail.com>
2023-06-11 21:38:06 -0500

    Added views and url for menu and booking

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from restaurant.views import BookingViewSet
+
+router = DefaultRouter()
+router.register(r'tables', BookingViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('restaurant/', include('restaurant.urls'))
+    path('restaurant/menu/', include('restaurant.urls')),
+    path('restaurant/booking/', include(router.urls)),
 ]
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from restaurant.views import index
+from restaurant.views import index, MenuItemsView, SingleMenuItemView
 
 urlpatterns = [
     path("", index, name='home'),
+    path('items/', MenuItemsView.as_view()),
+    path('items/<int:pk>', SingleMenuItemView.as_view()),
 ]
